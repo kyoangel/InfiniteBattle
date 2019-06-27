0bfaea18dd9728203e9888e7857a419fbc7325e7
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-06-27 22:50:05 +0800

    project init add Game action

--- InfiniteBattle/Controllers/HomeController.cs
+++ InfiniteBattle/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace InfiniteBattle.Controllers
 		{
 			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
 		}
+
+		public IActionResult Game()
+		{
+			return View();
+		}
 	}
 }
