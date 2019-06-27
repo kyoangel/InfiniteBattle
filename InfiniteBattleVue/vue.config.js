b6f63ced957fb5284b419996c1ecf08f8c1d971b
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-06-27 23:46:44 +0800

    setup complete

---
+++ InfiniteBattleVue/vue.config.js
@@ -0,0 +1,38 @@
+﻿const merge = require('webpack-merge')
+
+module.exports = {
+	configureWebpack: {
+		output: {
+			filename: "js/index.js"
+		},
+		devServer: {
+			open: true,
+			port: 8081
+		},
+
+	},
+	chainWebpack: config => {
+		config.module
+			.rule('images')
+			.test(/\.(jpg|png|gif|bmp|jpeg)$/)
+			.use('url-loader')
+			.loader('url-loader')
+			.tap(options => {
+				return merge(options, {
+					limit: 50000
+				})
+			})
+
+		const svgRule = config.module.rule('svg');
+		svgRule.uses.clear();
+		svgRule
+			.use('vue-svg-loader')
+			.loader('vue-svg-loader');
+	},
+	css: {
+		extract: {
+			filename: 'css/casinoWidget.css'
+		}
+	},
+	filenameHashing: false
+};
